9aae30bd369751f9979835e49ac018dc8520e014
hamdane1548 <oussamahamdane80@gmail.com>
2026-05-30 14:19:29 +0100

    Initial commit

---
+++ src/main/java/net/oussama/authflow_securedocs/AuthFlowSecureDocsApplication.java
@@ -0,0 +1,13 @@
+package net.oussama.authflow_securedocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFlowSecureDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthFlowSecureDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/oussama/authflow_securedocs/AuthFlowSecureDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.oussama.authflow_securedocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFlowSecureDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
